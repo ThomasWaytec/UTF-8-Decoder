779b86cafc0e4a68e50f88eadf49fe71abdf0953
ThomasWaytec <thomas.waytec@gmail.com>
2023-09-17 17:34:53 +0200

    restructure file reading

--- src/utf-8-decoder.c
+++ src/utf-8-decoder.c
@@ -5,13 +5,14 @@
 int main(void) {
     
     const char* FILE_NAME = "samples/basic/test.txt";
-
     FILE* file = fopen(FILE_NAME, "rb");
+
 
     /* get file size */
     fseek(file, 0, SEEK_END);
     const size_t FILE_SIZE = ftell(file); 
     fseek(file, 0, SEEK_SET);
+
 
     unsigned char current_byte;
     for (size_t i = 0; i < FILE_SIZE; i++) {
