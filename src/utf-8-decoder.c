52f65edd53e7659a0edc9cd66288541ded1f5e46
ThomasWaytec <thomas.waytec@gmail.com>
2023-09-20 22:52:43 +0200

    Unpack parse_non_initial_code_units function and rework count_leading_1s to extract_code_units_left and fix documentation

--- src/utf-8-decoder.c
+++ src/utf-8-decoder.c
@@ -4,22 +4,30 @@
 #define BYTE 8
 #define SHIFT (BYTE - 2) /* the base shift value for the code units. Allows to merge many code units into a single codde point */
 
-typedef unsigned int CodeUnit;
-typedef unsigned int CodePoint; /* has to be an int to check for EOF while reading file */
+typedef unsigned int CodeUnit;  /* 
+                                It has to be an int (even though code units are byte-sized) in order to:
+                                    - check for EOF with it while reading a file
+                                    - make it easily appendable to a code point
+                                */
+
+typedef unsigned int CodePoint;
 
 /* 
-Counts the number of continuous leading 1s in a code unit (in binary) 
+Extracts the number of code units left in a sequence
+by counting the number of continuous leading 1s (in binary) in the first code unit.
 (if it's the first code unit of a sequence,
-the number of leading 1s is equal to the number of code units left in the code unit sequence.)
+the number of code units left is equal to
+the number of leading 1s in the code unit sequence (minus one if the number of leading 1s are larger than 0))
 */
-size_t count_leading_1s(CodeUnit code_unit) {
-    size_t count = 0;
+size_t extract_code_units_left(CodeUnit code_unit) {
+    size_t code_units_left = 0;
 
     for (size_t i = BYTE - 1; code_unit & (1 << i); i--) {
-        count++;
+        code_units_left++;
     }
-        
-    return count;
+
+    if (code_units_left) {code_units_left -= 1;}
+    return code_units_left;
 }
 
 /*
@@ -40,23 +48,6 @@ void append_to_code_point(CodeUnit* code_unit, CodePoint* code_point, size_t cod
     *code_point = *code_point | (*code_unit << SHIFT*code_units_left);
 }
 
-/* extracts the code point from all the noninitial (meaning: not first) code units */
-void parse_nonitial_code_units(CodePoint* code_point, size_t code_units_left, FILE* file) {
-
-    CodeUnit current_code_unit;
-    while (code_units_left--)
-    {
-        /* parse code unit */
-        current_code_unit = fgetc(file);
-        trim_leading_1s(&current_code_unit);
-        append_to_code_point(&current_code_unit, code_point, code_units_left);
-        //*code_point = *code_point | (current_code_unit << SHIFT*code_units_left);   
-
-    }
-
-}
- 
-
 
 int main(void) {
     
@@ -68,31 +59,34 @@ int main(void) {
     fseek(file, 0, SEEK_END);
     const size_t FILE_SIZE = ftell(file); 
     fseek(file, 0, SEEK_SET);
-    printf("file_size=%d\n\n", FILE_SIZE);
 
 
     CodePoint current_code_point;
     size_t code_units_left; /* number of code units left in the code unit sequence */
-    CodeUnit first_code_unit; 
+    CodeUnit first_code_unit;
+    CodeUnit noninitial_code_unit;
     while ((first_code_unit = fgetc(file)) != EOF)
     {
         /* reset code point */
         current_code_point = 0;
 
+
         /* parse first code unit */
-        code_units_left = count_leading_1s(first_code_unit);
+        code_units_left = extract_code_units_left(first_code_unit);
         trim_leading_1s(&first_code_unit);
-
-        if (code_units_left) {code_units_left -= 1;} /* subtract the first code unit which was just parsed */
-
-        /* add the parsed first code unit to the code point */
         append_to_code_point(&first_code_unit, &current_code_point, code_units_left);
-        //current_code_point = current_code_point | (first_code_unit << SHIFT*code_units_left); 
-        
-        /* parse and add to the code point the remaining code units if there are any */
-        if (code_units_left) {
-            parse_nonitial_code_units(&current_code_point, code_units_left, file);    
+
+
+
+        /* parse all the noninitial (meaning: not first) code units */
+        while (code_units_left--)
+        {
+            noninitial_code_unit = fgetc(file);
+            trim_leading_1s(&noninitial_code_unit);
+            append_to_code_point(&noninitial_code_unit, &current_code_point, code_units_left);
+    
         }
+
     
 
         printf("current_code_point=%d\n", current_code_point);
