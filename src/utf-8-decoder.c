499d799e5ce06b3b549ed9864f3155833043e2c2
ThomasWaytec <thomas.waytec@gmail.com>
2023-09-20 20:09:37 +0200

    UTF-8 parsing working. Requires clean-up.

--- src/utf-8-decoder.c
+++ src/utf-8-decoder.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 
 #define BYTE 8
+#define SHIFT (BYTE - 2) /* the shift value for the first code unit of a unit sequence (BYTE length - first code unit's length)*/
 
 typedef unsigned int CodeUnit;
-typedef unsigned int CodePoint;
-
+typedef unsigned int CodePoint; /* has to be an int to check for EOF while reading file */
 
 /*
-Counts the number of continuous leading 1s in a code unit and trims them. (in binary) 
+Counts the number of continuous leading 1s in a code unit and trims those 1s. (in binary) 
 (if it's the first code unit of a sequence,
 the number of leading 1s is equal to the number of code units left in the code unit sequence.)
 */
-size_t parse_code_unit(CodeUnit* byte) {
+size_t parse_code_unit(CodeUnit* code_unit) {
     size_t count = 0;
 
-    for (size_t i = BYTE - 1; *byte & (1 << i); i--) {
+    for (size_t i = BYTE - 1; *code_unit & (1 << i); i--) {
         count++;
-        *byte &= ~(1 << i); // trim leading 1s
+        *code_unit &= ~(1 << i); /* trim leading 1s */
     }
         
     return count;
 }
 
-
-parse_nonitial_code_units(CodePoint* code_point, size_t code_units_left, FILE* file) {
+/* extracts the code point from all the noninitial (meaning: not first) code units */
+void parse_nonitial_code_units(CodePoint* code_point, size_t code_units_left, FILE* file) {
 
     CodeUnit current_code_unit;
     while (code_units_left--)
     {
         /* parse code unit */
         current_code_unit = fgetc(file);
+        printf("    current_code_unit=%d\n", current_code_unit);
         parse_code_unit(&current_code_unit);
         printf("    current_code_unit=%d\n", current_code_unit);
         printf("    code_point=%d\n", *code_point);
-        *code_point = *code_point | (current_code_unit << code_units_left*BYTE);   
+        //*code_point = *code_point | (current_code_unit << BYTE*code_units_left + 1);   
+        *code_point = *code_point | (current_code_unit << SHIFT*code_units_left);   
+
     }
 
 }
@@ -54,27 +57,31 @@ int main(void) {
     printf("file_size=%d\n\n", FILE_SIZE);
 
 
+    CodeUnit first_code_unit; 
+    size_t code_units_left; /* number of code units left in the code unit sequence */
     CodePoint current_code_point;
-    CodeUnit first_code_unit; // has to be an int to check for EOF
-    size_t code_units_left; // number of code units left in the code unit sequence
-
-    
     while ((first_code_unit = fgetc(file)) != EOF)
     {
 
+        current_code_point = 0;
         /* parse 1st byte of code unit sequence */
         printf("first_code_unit=%d\n", first_code_unit);
         code_units_left = parse_code_unit(&first_code_unit);
 
-        printf("code_units_left=%d\n", code_units_left);
-        printf("first_code_unit=%d\n", first_code_unit);
-        current_code_point = first_code_unit << (BYTE - code_units_left);
-        printf("current_code_point=%d\n", current_code_point);
+        if (code_units_left) {code_units_left -= 1;} /* subtract the first code unit which was just parsed */
 
+        /* add the parsed first code unit to the code point */
+        current_code_point = current_code_point | (first_code_unit << (SHIFT*code_units_left)); 
+        
+        //printf("code_units_left=%d\n", code_units_left);
+        printf("first_code_unit=%d\n", first_code_unit);
+        //printf("current_code_point=%d\n", current_code_point);
+
+        /* parse and add to the code point the remaining code units if there are any */
         if (code_units_left) {
-            code_units_left -= 1; // subtract the first code unit which was just parsed
             parse_nonitial_code_units(&current_code_point, code_units_left, file);    
         }
+    
 
         printf("current_code_point=%d\n", current_code_point);
         printf("\n");
@@ -82,14 +89,6 @@ int main(void) {
 
         
     }
-
-    /* merge add n to code point */
-    // after all the trimming has been done
-
-    /* merge bytes */
-    //unsigned int code_point;
-    //code_point = (code_point << 8) | n;
-    //printf("%d\n", code_point);
 
     return 0;
 }
